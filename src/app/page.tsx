9d34f42537efdc132365f54b9cbf655be157def9
omor github <omorfaruque783@gmail.com>
2025-11-11 00:54:07 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@
 import Myaccordion from './components/Accordion'
 import Myalert from './components/Alert'
 import Myautocomplete from './components/Autocomplete'
+import Avatars from './components/Avatar'
 import NavBar from './components/Navbar'
 
 export default function Home() {
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div className=''>
       <NavBar />
       <Myaccordion />
       <Myautocomplete />
       <Myalert />
+      <div className="max-w-7xl mx-auto justify-center items-center">
+      <Avatars/>
+      </div>
     </div>
+
   )
 }
